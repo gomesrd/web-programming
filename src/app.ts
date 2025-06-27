d1c342239dc0546886655aa42b603f5288b47c13
douglas.santos <douglaspg@outlook.com>
2025-06-27 19:20:20 -0300

    refactor: update server response message and change default port

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express, {Request, Response, NextFunction} from "express"
 import dotenv from "dotenv"
-dotenv.config()
-
 import patientRouter from "./routers/PatientRouter"
+
+dotenv.config()
 
 const app = express()
 
@@ -11,10 +11,10 @@ app.use(express.json());
 app.use("/patients", patientRouter);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-    res.send("Hello me, it's me again...");
+    res.send("Welcome to the Patient Management API");
 });
 
-const PORT = process.env.PORT || 12345 //define o 12345 como default
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
